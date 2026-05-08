9653986f94228239e792f585fff7ba30c12097c2
dunay <dunayev.artem@gmail.com>
2026-05-08 13:24:34 +0300

    move helpers to util_test

--- tests/model_test.go
+++ tests/model_test.go
@@ -18,48 +18,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func requireErrorIsNoPanic(t *testing.T, expected error, f func() error) {
-	t.Helper()
-	var err error
-	require.NotPanics(t, func() {
-		err = f()
-	})
-	require.ErrorIs(t, err, expected)
-
-}
-
-func requireBalanceErrorIsNoPanic(t *testing.T, expected error, f func() (int64, error)) {
-	t.Helper()
-	var err error
-	require.NotPanics(t, func() {
-		_, err = f()
-	})
-	require.ErrorIs(t, err, expected)
-
-}
-
-func requireErrorContainsNoPanic(t *testing.T, expected string, f func() error) {
-	t.Helper()
-
-	var err error
-	require.NotPanics(t, func() {
-		err = f()
-	})
-	require.Error(t, err)
-	require.ErrorContains(t, err, expected)
-}
-
-func requireBalanceErrorContainsNoPanic(t *testing.T, expected string, f func() (int64, error)) {
-	t.Helper()
-
-	var err error
-	require.NotPanics(t, func() {
-		_, err = f()
-	})
-	require.Error(t, err)
-	require.ErrorContains(t, err, expected)
-}
-
 func TestEmptyBank(t *testing.T) {
 	t.Parallel()
 
